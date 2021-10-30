bcf3ba3e5f5b37709547435b08c670f71aa653ee
Neil Sabde <neilsabde@gmail.com>
2021-10-30 15:49:16 +0530

    Fifth Commit

--- markerHandler.js
+++ markerHandler.js
@@ -30,6 +30,7 @@ AFRAME.registerComponent("marker-handler", {
       bdiv.style.display = "flex";
       let orderButton = document.getElementById("order-button");
       let summaryButton = document.getElementById("summary-button");
+      let payButton = document.getElementById("pay-button");
       orderButton.addEventListener("click", () => {
         firebase
           .firestore()
@@ -69,12 +70,9 @@ AFRAME.registerComponent("marker-handler", {
         });
       });
       summaryButton.addEventListener("click", () => {
-        swal({
-          icon: "warning",
-          title: "Order Summary",
-          text: "WIP",
-        });
+        this.handleOrderSummary();
       });
+      payButton.addEventListener("click", () => this.handlePayment());
     } else {
       swal("Oops :(", "Toy is out of stock!", "error");
     }
@@ -89,5 +87,80 @@ AFRAME.registerComponent("marker-handler", {
       .collection("toys")
       .get()
       .then((snap) => snap.docs.map((doc) => doc.data()));
+  },
+  getOrderSummary: async function (uid) {
+    return await firebase
+      .firestore()
+      .collection("users")
+      .doc(uid)
+      .get()
+      .then((doc) => doc.data());
+  },
+  handleOrderSummary: async function () {
+    let modalDiv = document.getElementById("modal-div");
+    modalDiv.style.display = "flex";
+    uid = uid;
+    let orderSummary = await this.getOrderSummary(uid);
+    let tbody = document.getElementById("bill-table-body");
+    tbody.innerHTML = "";
+    let currentOrders = Object.keys(orderSummary.current_orders);
+    currentOrders.map((i) => {
+      let tr = document.createElement("tr");
+      let item = document.createElement("td");
+      let price = document.createElement("td");
+      let quantity = document.createElement("td");
+      let subtotal = document.createElement("td");
+      item.innerHTML = orderSummary.current_orders[i].item;
+      price.innerHTML = "$" + orderSummary.current_orders[i].price;
+      price.setAttribute("class", "text-center");
+      quantity.innerHTML = orderSummary.current_orders[i].quantity;
+      quantity.setAttribute("class", "text-center");
+      subtotal.innerHTML = "$" + orderSummary.current_orders[i].subtotal;
+      subtotal.setAttribute("class", "text-center");
+      tr.appendChild(item);
+      tr.appendChild(price);
+      tr.appendChild(quantity);
+      tr.appendChild(subtotal);
+      tbody.appendChild(tr);
+    });
+    let totalTr = document.createElement("tr");
+    let td1 = document.createElement("td");
+    td1.setAttribute("class", "no-line");
+    let td2 = document.createElement("td");
+    td1.setAttribute("class", "no-line");
+    let td3 = document.createElement("td");
+    td1.setAttribute("class", "no-line text-cente");
+    let strong = document.createElement("strong");
+    strong.innerHTML = "Total";
+    td3.appendChild(strong);
+    let td4 = document.createElement("td");
+    td1.setAttribute("class", "no-line text-right");
+    td4.innerHTML = "$" + orderSummary.total_bill;
+    totalTr.appendChild(td1);
+    totalTr.appendChild(td2);
+    totalTr.appendChild(td3);
+    totalTr.appendChild(td4);
+    tbody.appendChild(totalTr);
+  },
+  handlePayment: function () {
+    document.getElementById("modal-div").style.display = "none";
+    uid = uid.toUpperCase();
+    firebase
+      .firestore()
+      .collection("users")
+      .doc(uid)
+      .update({
+        current_orders: {},
+        total_bill: 0,
+      })
+      .then(() => {
+        swal({
+          icon: "success",
+          title: "Thanks For Paying!",
+          text: "We Hope You Like Your Toy!",
+          timer: 2500,
+          buttons: false,
+        });
+      });
   },
 });
